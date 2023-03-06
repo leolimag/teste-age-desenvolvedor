5de3b2bf9348b3a406005a58a528cec42d82e11a
Leonardo Lima <leonardo.lima.gomes2003@gmail.com>
2023-03-06 14:26:43 -0300

    criando a model funcionario

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,25 @@
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import br.com.empresa.projeto.business.ExameBusiness;
 import br.com.empresa.projeto.connection.ConnectionFactory;
 import br.com.empresa.projeto.model.Exame;
-import br.com.empresa.projeto.model.ExameDAO;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try (Connection con = new ConnectionFactory().getConnection()){
-			Exame exame = new Exame("exame 7", "13/11/2020");
-			ExameDAO dao = new ExameDAO(con);
-			dao.findAll().forEach(System.out::println);
-			//dao.insert(exame);
+			Exame exame = new Exame("exame 10", "30/11/2020");
+			ExameBusiness exameBusiness = new ExameBusiness();
+			exameBusiness.getAll().forEach(System.out::println);
+			//exameBusiness.insert(exame);
 			System.out.println("\ninserção realizada com sucesso\n");
-			dao.findAll().forEach(System.out::println);
+			exameBusiness.getAll().forEach(System.out::println);
 			System.out.println("\nexclusão realizada com sucesso\n");
-			exame.setId(6);
-			dao.delete(exame);
-			dao.findAll().forEach(System.out::println);
+			exame.setId(8);
+			//exameBusiness.update(exame);
+			exameBusiness.delete(exame);
+			exameBusiness.getAll().forEach(System.out::println);
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
---
+++ src/main/java/br/com/empresa/projeto/model/Funcionario.java
@@ -0,0 +1,32 @@
+package br.com.empresa.projeto.model;
+
+public class Funcionario {
+	
+	private Integer id;
+	private String nome;
+	
+	public Funcionario(Integer id, String nome) {
+		this.nome = nome;
+		this.id = id;
+	}
+	public Funcionario(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "Exame [id=" + id + ", nome=" + nome + "]";
+	}
+	
+}
