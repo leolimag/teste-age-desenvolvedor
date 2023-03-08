fffd695fcc841ebad14f5b4229a796d43d3bf15e
Leonardo Lima <leonardo.lima.gomes2003@gmail.com>
2023-03-08 14:46:41 -0300

    criando confirmador para o funcionario

--- src/main/webapp/script/confirm.js
+++ src/main/webapp/script/confirm.js
@@ -1,6 +1,13 @@
-function confirmador(id) {
+function confirmadorExame(id) {
 	let answer = confirm("Deseja realmente excluir este exame?");
-	if(answer === true){
+	if (answer === true) {
 		window.location.href = "deletaExame?id=" + id;
+	}
+}
+
+function confirmadorFuncionario(id) {
+	let answer = confirm("Deseja realmente excluir este funcionário?");
+	if (answer === true) {
+		window.location.href = "deletaFuncionario?id=" + id;
 	}
 }
